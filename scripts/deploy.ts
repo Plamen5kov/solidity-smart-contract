1cff14539cde880fa6101fa541edc18fefec5378
Plamen Petkov <plamen.krasimirov.petkov@gmail.com>
2022-06-30 12:05:26 +0300

    simple lottery game with lottery ticket implementation

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,30 +1,32 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  //get a user account to buy a ticket
+  const signers = await ethers.getSigners();
+  const ticketHolder = signers[1]
 
-  await greeter.deployed();
+  //deploy lottery game
+  const LotteryGameFactory = await ethers.getContractFactory("LotteryGame")
+  const lotteryGameContract = await LotteryGameFactory.deploy()
 
-  console.log("Greeter deployed to:", greeter.address);
+  //attach filter for lotery events
+  lotteryGameContract
+    .on("TicketCreated", async (to: any, amount: any, from: any) => {
+      // console.log(to, amount, from);
+      console.log("TicketCreated")
+    });
+
+  //connect to the lotery contract
+  const connectedGameContract = lotteryGameContract.connect(ticketHolder)
+
+  //buy a ticket for atleast 0.1 ether
+  const options = { value: ethers.utils.parseEther("0.011") }
+  connectedGameContract.enter(options)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
